e13e406c481b444f6d0fd796a7ba54971f37a1c3
Konstantin Shilkov <konst.shilkov@gmail.com>
2023-01-23 02:19:46 +0100

    tuto-7 fini

--- src/Entity/Categories.php
+++ src/Entity/Categories.php
@@ -21,6 +21,9 @@ use SlugTrait;
     #[ORM\Column(length: 100)]
     private ?string $name = null;
 
+    #[ORM\Column]
+    private ?int $categoryOrder = null;
+
     #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'categories')]
     #[ORM\JoinColumn(onDelete:'CASCADE')]
     private ?self $parent = null;
@@ -50,6 +53,18 @@ use SlugTrait;
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCategoryOrder(): ?int
+    {
+        return $this->categoryOrder;
+    }
+
+    public function setCategoryOrder(int $categoryOrder): self
+    {
+        $this->name = $categoryOrder;
 
         return $this;
     }
